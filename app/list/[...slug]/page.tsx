6fe83ed4b4d2dadc34ef5baadcf901d4d645cda3
shelmain <2205744611@qq.com>
2025-06-04 00:02:23 +0800

    feat: 历史记录

---
+++ app/list/[...slug]/page.tsx
@@ -0,0 +1,76 @@
+"use client"
+import React, {useEffect, useState} from 'react';
+import { Tag } from 'antd';
+
+interface QuestionItem {
+  序号: string;
+  题目板块: string;
+  难度系数: string;
+  题目内容: string;
+  题目答案: string;
+  选项: string[];
+  题目解析: string;
+  文件根据: string;
+  yourAnswer: string[];
+  isCorrect: boolean | null;
+}
+
+const QuestionListPage: React.FC = (props:{params:{slug:string[]}}) => {
+  const [type,page=0] = React.use(props?.params)?.slug;
+  const [data,setData] = useState([]);
+  // const [typeKeys,setTypeKeys] = useState([]);
+  useEffect(() => {
+    let typekeys =""
+    if(type === "multiple"){
+      typekeys = "yourMultipleAnswer"
+    }else{
+      typekeys = "yourAnswer"
+    }
+    // const orderNumber = localStorage.getItem("singleNumber") ? JSON.parse(localStorage.getItem("singleNumber") || ""):0;
+    const historyAnswer = JSON.parse(localStorage.getItem(typekeys+page) || "[]");
+    setData(historyAnswer);
+
+  }, []);
+  // const data: QuestionItem[] = JSON.parse(localStorage.getItem("singleData")|| '[]')
+
+  const getBackgroundColor = (isCorrect: boolean | null) => {
+    if (isCorrect === true) return 'bg-green-100 border-green-500';
+    if (isCorrect === false) return 'bg-red-100 border-red-500';
+    return 'bg-gray-100 border-gray-500';
+  };
+  if(data.length){
+    return <div className="flex items-center">暂无内容。。。</div>
+  }
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-xl font-bold mb-6 ">题目列表</h1>
+      <div className="space-y-4 text-[#000]">
+        {data.map((item) => (
+          <div
+            key={item.序号}
+            className={`p-4 rounded-lg border-l-4 text-block ${getBackgroundColor(item.isCorrect)}`}
+          >
+            <div className="flex text-block justify-between items-start mb-2">
+              <div className="mb-2">
+                <span className="font-bold text-block">{item.序号}.</span>
+                <span className="font-medium text-block text-Text12">{item.题目内容}</span>
+              </div>
+              {/*<div className="flex space-x-2 text-block">*/}
+              {/*  <Tag color="blue">{item.题目板块}</Tag>*/}
+              {/*  <Tag color={item.难度系数 === '困难' ? 'red' : item.难度系数 === '中等' ? 'orange' : 'green'}>*/}
+              {/*    {item.难度系数}*/}
+              {/*  </Tag>*/}
+              {/*</div>*/}
+            </div>
+
+            <div className="text-sm text-gray-500">
+              你的答案: {Array.isArray(item.yourAnswer) ? item.yourAnswer.join(', '): item.yourAnswer || '未回答'}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default QuestionListPage;
--- app/page.tsx
+++ app/page.tsx
@@ -38,12 +38,12 @@ export default function Home() {
     if (token) {
         return (
             <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-sky-500 to-emerald-500 text-white">
-                <div className="bg-opacity-80 bg-black p-8 rounded-lg shadow-lg max-w-md w-full">
+                <div className="bg-opacity-80  p-8 rounded-lg shadow-lg max-w-md w-full">
                     <h1 className="text-4xl font-bold text-center mb-6">招标代理从业人员培训题库</h1>
                     <p className="text-lg text-center mb-8">
                         欢迎来到招标代理从业人员培训题库！这里提供了丰富的单选题和多选题，帮助你提升专业知识和技能。祝你学习愉快！
                     </p>
-                    <div className="flex justify-center space-x-4">
+                    <div className="flex justify-center space-x-4 gap-2 flex-row flex-wrap">
                         <Link href="/single_choice" passHref>
                             <Button type="primary" size="large" className="bg-blue-500 hover:bg-blue-700 text-white">
                                 单选题
@@ -52,6 +52,16 @@ export default function Home() {
                         <Link href="/multiple_choice" passHref>
                             <Button type="primary" size="large" className="bg-green-500 hover:bg-green-700 text-white">
                                 多选题
+                            </Button>
+                        </Link>
+                        <Link href="/orderList/multiple" passHref>
+                            <Button type="primary" size="large" className="bg-green-500 hover:bg-green-700 text-white">
+                                多选题刷题记录
+                            </Button>
+                        </Link>
+                        <Link href="/orderList/single" passHref>
+                            <Button type="primary" size="large" className="bg-green-500 hover:bg-green-700 text-white">
+                                单选题刷题记录
                             </Button>
                         </Link>
                     </div>
