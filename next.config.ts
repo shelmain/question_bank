2755ec244b0f6061cb9c5b6c88d459a9de065e81
apple <2205744611@qq.com>
2025-06-05 14:54:33 +0800

    fix: 网络环境差的情况下打开白屏

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,12 @@ const nextConfig: NextConfig = {
         // 禁用 ESLint 检查
         ignoreDuringBuilds: true,
     },
+    webpack: (config) => {
+        config.output.chunkFilename = `static/chunks/[name].[contenthash].js`;
+        return config;
+    },
+    // 添加资产前缀（如果使用CDN）
+    assetPrefix: process.env.CDN_URL || '',
   /* config options here */
 };
 
